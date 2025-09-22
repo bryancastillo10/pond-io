7fa270595829af78108a72213f49ed7a3a9db538
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2025-09-23 05:15:27 +0800

    🎨 style: enhanced variants of button

--- client/src/components/pages/ModelsOverview.tsx
+++ client/src/components/pages/ModelsOverview.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "@/components/navigation/Sidebar";
 
+import Button from "@/components/ui/Button";
+
 const ModelsOverview = () => {
   return (
     <div className="flex overflow-x-auto duration-500 ease-in-out w-full h-screen bg-background text-text">
@@ -9,6 +11,13 @@ const ModelsOverview = () => {
         <p>Add Drawer component</p>
 
         <p>Models Home Page</p>
+
+        <div className="grid grid-cols-2 gap-4 my-4">
+          <Button variant="primary">Primary Button</Button>
+          <Button variant="outline">Secondary Button</Button>
+          <Button variant="danger">Cancel Button</Button>
+          <Button>Default Button</Button>
+        </div>
         {/* Need to customize routing for dynamic rendering of the nested route */}
         <Outlet />
       </div>
