c96fa013c734843adea3105d079d2149ad6f38af
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-09-22 15:43:56 +0800

    ✨plan: initial app layout at models page

---
+++ client/src/components/navigation/Sidebar.tsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+  return (
+    <aside className="flex flex-col transform transition-width duration-500 bg-primary text-background">
+      Sidebar Here
+    </aside>
+  );
+};
+
+export default Sidebar;
