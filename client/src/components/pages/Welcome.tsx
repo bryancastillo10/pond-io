cb9717c47470cccf33d637645ea38971019978ce
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-09-22 14:49:35 +0800

    🧩 component: Button

--- client/src/components/pages/Welcome.tsx
+++ client/src/components/pages/Welcome.tsx
@@ -1,7 +1,20 @@
+import Button from "@/components/ui/Button";
+import { Link } from "react-router-dom";
+
 const Welcome = () => {
   return (
     <div className="w-full h-screen bg-dark-background text-dark-primary">
-      <h1 className="text-2xl p-4">Welcome to the App</h1>
+      <div className="flex flex-col h-screen justify-center gap-2 items-center">
+        <h1 className="text-5xl font-semibold font-heading italic">Pond.io</h1>
+        <p className="font-body text-background text-xl">
+          An open-source wastewater treatment plant simulator
+        </p>
+        <div className="mt-6">
+          <Button>
+            <Link to="/model">Get Started ➡️</Link>
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/components/ui/Button.tsx
+++ client/src/components/ui/Button.tsx
@@ -1,5 +1,58 @@
-const Button = () => {
-  return <button>Button</button>;
+interface ButtonProps {
+  action?: () => void;
+  type?: "submit" | "reset" | "button";
+  width?: string;
+  loading?: boolean;
+  disabled?: boolean;
+  fontSize?: string;
+  variant?: string;
+  children: React.ReactNode;
+}
+
+const Button = ({
+  action,
+  type = "button",
+  width = "w-fit",
+  fontSize = "text-base",
+  loading,
+  disabled,
+  variant,
+  children,
+}: ButtonProps) => {
+  const getBtnStyle = (variant: string) => {
+    switch (variant) {
+      case "primary":
+        return "border border-transparent bg-dark-primary text-white hover:bg-primary/70";
+      case "outline":
+        return "border text-primary border-primary hover:border-primary  hover:bg-primary hover:text-white";
+      case "danger":
+        return "border border-transparent text-white bg-rose-500 hover:bg-rose-700";
+      default:
+        return "bg-cyan-600 text-slate-100  hover:bg-teal-500/90 p-0";
+    }
+  };
+  return (
+    <button
+      onClick={action}
+      disabled={disabled}
+      type={type}
+      className={`${width} ${fontSize} font-semibold px-3 py-2 rounded-lg disabled:bg-neutral
+        duration-500 ease-in-out
+        ${getBtnStyle(variant!)}`}
+    >
+      {loading ? (
+        <div className="flex justify-center items-center gap-0.5">
+          <svg
+            className="animate-spin size-4 mr-2 border-white border-t-outline border-t-darkGray border-2 rounded-full"
+            viewBox="0 0 24 24"
+          />
+          <span className="font-semibold text-base">Loading . . .</span>
+        </div>
+      ) : (
+        children
+      )}
+    </button>
+  );
 };
 
 export default Button;
