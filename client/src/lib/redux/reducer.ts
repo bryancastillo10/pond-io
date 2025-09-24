e1e010e386ba37127bac8f041c73f31e327e9f9d
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-09-24 15:38:45 +0800

    🍕 redux slice: theme and drawer

--- client/src/lib/redux/reducer.ts
+++ client/src/lib/redux/reducer.ts
@@ -1,5 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
-const rootReducer = combineReducers({});
+// UI States
+import themeReducer from "@/lib/redux/slice/themeSlice";
+import drawerReducer from "@/lib/redux/slice/drawerSlice";
+
+const rootReducer = combineReducers({
+  theme: themeReducer,
+  drawer: drawerReducer,
+});
 
 export default rootReducer;
---
+++ client/src/lib/redux/slice/drawerSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+interface DrawerState {
+  isOpenDrawer: boolean;
+  title: string;
+  componentName: string | null;
+  bodyProps?: Record<string, any>;
+}
+
+const initialState: DrawerState = {
+  isOpenDrawer: false,
+  title: "",
+  componentName: "",
+  bodyProps: {},
+};
+
+export const drawerSlice = createSlice({
+  name: "drawer",
+  initialState,
+  reducers: {
+    openDrawer(
+      state,
+      action: PayloadAction<{
+        title: string;
+        componentName: string;
+        bodyProps: object;
+      }>
+    ) {
+      state.isOpenDrawer = true;
+      state.title = action.payload.title;
+      state.componentName = action.payload.componentName;
+      state.bodyProps = action.payload.bodyProps;
+    },
+    closeDrawer(state) {
+      state.isOpenDrawer = false;
+      state.title = "";
+      state.componentName = null;
+    },
+  },
+});
+
+export const { openDrawer, closeDrawer } = drawerSlice.actions;
+
+export default drawerSlice.reducer;
