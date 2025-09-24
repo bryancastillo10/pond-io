6f4af5406ee92bdcc2ff89276252b37646275ee0
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-09-24 14:34:52 +0800

    ✨ component: home page

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import {
   Welcome,
+  Home,
   ModelSimulation,
   About,
   Documentation,
@@ -13,7 +14,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Welcome />} />
       <Route element={<AppLayout />}>
-        <Route path="/model" index />
+        <Route path="/model" index element={<Home />} />
         <Route path="/model/:name" element={<ModelSimulation />} />
         <Route path="/about" element={<About />} />
         <Route path="/docs" element={<Documentation />} />
--- client/src/components/pages/index.ts
+++ client/src/components/pages/index.ts
@@ -1,6 +1,7 @@
 import Welcome from "./Welcome";
+import Home from "./Home";
 import ModelSimulation from "./ModelSimulation";
 import About from "./About";
 import Documentation from "./Documentation";
 
-export { Welcome, ModelSimulation, About, Documentation };
+export { Welcome, Home, ModelSimulation, About, Documentation };
