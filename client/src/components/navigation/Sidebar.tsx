cb03eaf3341859ea0eb4806e0cc2cc7663503bf8
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-09-24 14:29:43 +0800

    🧩 component: ModelItem with transitions

--- client/src/components/navigation/Sidebar.tsx
+++ client/src/components/navigation/Sidebar.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import AppLogo from "@/assets/applogo.png";
 import { Box, ChevronRight } from "lucide-react";
 
-import { sidebarItems } from "@/components/navigation/sidebaritem";
+import { sidebarItems, modelItems } from "@/components/navigation/constants";
 import SidebarItem from "@/components/navigation/SidebarItem";
+import ModelItem from "./ModelItem";
 
 const Sidebar = () => {
+  const [showModelList, setShowModelList] = useState<boolean>(false);
+
   const currentYear = new Date().getFullYear();
+
+  const toggleShowModel = () => {
+    setShowModelList(!showModelList);
+  };
 
   return (
     <aside className="flex flex-col transform transition-width duration-500 bg-gradient-to-b from-primary to-accent text-background">
@@ -21,21 +29,36 @@ const Sidebar = () => {
             />
           ))}
 
-          <div className="flex justify-between items-center px-2 rounded-2xl my-4 w-full gap-2">
+          <div
+            onClick={toggleShowModel}
+            className="flex justify-between items-center cursor-pointer px-2 py-1 rounded-2xl my-4 w-full hover:bg-accent duration-500 ease-out gap-2"
+          >
             <div className="flex items-center gap-2">
               <Box size={24} />
               <p className="font-semibold">Models</p>
             </div>
             <div className="mr-2">
-              <ChevronRight />
+              <ChevronRight
+                className={`${
+                  showModelList ? "rotate-90" : "rotate-0"
+                } duration-500 ease-out`}
+              />
             </div>
           </div>
 
-          <ul className="flex flex-col gap-2 px-8">
-            <li>MBBR</li>
-            <li>AD</li>
-            <li>Septic Tank</li>
-            <li>UASB</li>
+          <ul
+            className={`flex flex-col gap-2 px-8 py-2 overflow-hidden transition-all duration-500 ease-in-out
+    ${showModelList ? "opacity-100" : "opacity-0"}
+  `}
+          >
+            {modelItems.map((model) => (
+              <ModelItem
+                key={model.id}
+                showModelList={showModelList}
+                link={model.link}
+                name={model.name}
+              />
+            ))}
           </ul>
         </div>
 
