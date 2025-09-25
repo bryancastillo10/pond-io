f9fc1b3ae610639b94702eff5bda8e1ab61bfbc2
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-09-25 13:06:38 +0800

    🧹 chore: page not found layout

--- client/src/app/PageNotFound.tsx
+++ client/src/app/PageNotFound.tsx
@@ -1,8 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import { useAppSelector } from "@/lib/redux/hooks";
+
+import Button from "@/components/ui/Button";
+
 const PageNotFound = () => {
+  const darkMode = useAppSelector((state) => state.theme.isDarkMode);
+
+  const navigate = useNavigate();
+
   return (
-    <section className="flex flex-col gap-2 justify-center items-center w-full h-screen">
-      <h1>Sorry, Page Not Found</h1>
-      <p>Go Back To Home</p>
+    <section
+      className={`flex flex-col gap-2 justify-center items-center w-full h-screen
+          ${
+            darkMode
+              ? "bg-dark-background text-dark-text"
+              : "bg-background text-text"
+          }
+      `}
+    >
+      <h1 className="font-semibold">Sorry, Page Not Found ☹️</h1>
+      <div className="mt-4">
+        <Button action={() => navigate("/model")} variant="primary">
+          Go Back To Home
+        </Button>
+      </div>
     </section>
   );
 };
