829aa56bbffd6138eb9da0d987b35dcfccfc2bdb
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-09-30 10:54:54 +0800

    🧩 components: Card

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,7 +1,34 @@
+import TextHeader from "@/components/static/TextHeader";
+
+import Card from "@/components/ui/Card";
+
+import { modelsList } from "@/components/ui/constants";
+
 const Home = () => {
   return (
-    <section>
-      <h1>Home Page</h1>
+    <section className="w-full h-full flex flex-col gap-2">
+      <TextHeader text="Launch your Design" />
+      <p className="text-balance leading-relaxed">
+        Welcome to Pond.io! Use the interactive models below to simulate,
+        analyze, and design wastewater treatment plant (WWTP) scenarios. Simply
+        click on an available model such as MBBR or Anaerobic Digestion—to begin
+        adjusting parameters and observing real-time results based on your
+        inputs.
+      </p>
+
+      <div className="grid grid-cols-2 gap-2">
+        {modelsList.map((model) => (
+          <Card
+            key={model.id}
+            title={model.title}
+            description={model.description}
+            category={model.category}
+            expectedResults={model.expectedResults}
+            imageSrc={model.image}
+            imageAlt={model.imageAlt}
+          />
+        ))}
+      </div>
     </section>
   );
 };
---
+++ client/src/components/ui/constants.ts
@@ -0,0 +1,50 @@
+import MbbrImg from "@/assets/mbbr.png";
+
+export const modelsList = [
+  {
+    id: 1,
+    title: "MBBR (Moving Bed Biofilm Reactor)",
+    description:
+      "A biological wastewater treatment process that uses free-floating plastic carriers with high surface area for biofilm growth. The carriers move freely in the reactor, providing efficient organic matter and nitrogen removal through aerobic biological processes",
+    category: "Biological Treatment",
+    expectedResults: [
+      "Loading Rates",
+      "Tank Volumes",
+      "Media Surface Area",
+      "Tank Dimensions",
+    ],
+    image: MbbrImg,
+    imageAlt: "MBBR Treatment Model",
+  },
+
+  //   {
+  //     id: 2,
+  //     title: "MBBR (Moving Bed Biofilm Reactor)",
+  //     description:
+  //       "A biological wastewater treatment process that uses free-floating plastic carriers with high surface area for biofilm growth. The carriers move freely in the reactor, providing efficient organic matter and nitrogen removal through aerobic biological processes",
+  //     category: "Biological Treatment",
+  //     expectedResults: [
+  //       "Loading Rates",
+  //       "Tank Volumes",
+  //       "Media Surface Area",
+  //       "Tank Dimensions",
+  //     ],
+  //     image: MbbrImg,
+  //     imageAlt: "MBBR Treatment Model",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "MBBR (Moving Bed Biofilm Reactor)",
+  //     description:
+  //       "A biological wastewater treatment process that uses free-floating plastic carriers with high surface area for biofilm growth. The carriers move freely in the reactor, providing efficient organic matter and nitrogen removal through aerobic biological processes",
+  //     category: "Biological Treatment",
+  //     expectedResults: [
+  //       "Loading Rates",
+  //       "Tank Volumes",
+  //       "Media Surface Area",
+  //       "Tank Dimensions",
+  //     ],
+  //     image: MbbrImg,
+  //     imageAlt: "MBBR Treatment Model",
+  //   },
+];
