051b0c4c67d3c56f7f774d9d78696c17c1347620
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-10-03 10:58:42 +0800

    🔰init: mbbr simulation dto,handler,& service

---
+++ server/internal/mbbr/dto.go
@@ -0,0 +1,34 @@
+package mbbr
+
+type SimulateMbbrRequest struct {
+	FlowRate float64 `json:"flowRate"`   //m3/d
+	InfBOD float64 `json:"infBOD"`    // mg/L
+	InfTKN float64 `json:"infTKN"`    // mg N/L
+	TargetEffPercBOD float64 `json:"targetEffPercBOD"`  // %
+	TargetEffPercTKN float64	`json:"targetEffPercTKN"`  // %
+	FirstStage MBBRSpecifications  
+	SecondStage MBBRSpecifications
+}
+
+type MBBRSpecifications struct  {
+	SALR float64 `json:"salr"`  // g/m2-d
+	CarrierFill float64 `json:"carrierFill"`  //%
+	WaterLevel float64 `json:"waterLevel"`   //m
+	CarrierSurfaceArea float64 `json:"carrierSurfaceArea"` //m2/m3
+	LengthWidthRatio float64 `json:"lengthWidthRatio"`
+}
+
+type SimulateMbbrResponse struct {
+	FirstStage DesignResult
+	SecondStage DesignResult
+}
+
+
+type DesignResult struct {
+	LoadingRate float64 `json:"loadingRate"`  //BOD LoadingRate for first stage & Ammonia Loading rate for second stage in g/d
+	MediaSurfaceArea float64 `json:"mediaSurfaceArea"` // m2
+	MediaVolume	float64 `json:"mediaVolume"`  // m3
+	TankVolume float64 `json:"tankVolume"`  // m3
+	Width float64`json:"width"`   // m
+	Length float64 `json:"length"` //m
+}
---
+++ server/internal/mbbr/handler.go
@@ -0,0 +1,29 @@
+package mbbr
+
+import (
+	http_helper "pond-io-server/pkg/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	service *Service
+}
+
+func NewHandler() *Handler {
+	service := NewService()
+	return &Handler{ service: service}
+}
+
+
+func (h *Handler) SimulateMBBR(c *gin.Context) { 
+	req, err := http_helper.BindJSON[SimulateMbbrRequest](c)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	println(req)
+
+	c.JSON(200, gin.H{"message":"Simulation is successful"})
+}
