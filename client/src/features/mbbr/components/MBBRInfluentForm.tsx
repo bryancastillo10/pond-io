7e0b8ddfe31ba7237fb2d06c4480081177d1f625
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-10-15 14:24:46 +0800

    ✨ feature: mbbr form context

--- client/src/features/mbbr/components/MBBRInfluentForm.tsx
+++ client/src/features/mbbr/components/MBBRInfluentForm.tsx
@@ -1,9 +1,20 @@
+import { useMBBRFormContext } from "@/features/mbbr/context/MBBRFormContext";
+import NumberInput from "@/components/ui/NumberInput";
+
 const MBBRInfluentForm = () => {
+  const { infData, handleChange } = useMBBRFormContext();
+
   return (
     <div className="">
-      <h1>Inlfuent Form</h1>
-      <p>MBBR Influent Form</p>
-      <form></form>
+      <form>
+        <NumberInput
+          label="Flow Rate"
+          value={infData.flowRate}
+          onChange={handleChange("infData", "flowRate")}
+          disabled={false}
+          unit="m3/d"
+        />
+      </form>
     </div>
   );
 };
