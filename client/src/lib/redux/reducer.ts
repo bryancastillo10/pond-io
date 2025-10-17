cd54671fe6d6639c686478f648a794fb9ed0a831
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-10-17 15:38:15 +0800

    🌐 lib: redux api reducer path

--- client/src/lib/redux/reducer.ts
+++ client/src/lib/redux/reducer.ts
@@ -4,9 +4,17 @@ import { combineReducers } from "@reduxjs/toolkit";
 import themeReducer from "@/lib/redux/slice/themeSlice";
 import drawerReducer from "@/lib/redux/slice/drawerSlice";
 
+// API from RTK-Query
+import { mbbrApi } from "@/features/mbbr/api/simulate";
+
 const rootReducer = combineReducers({
   theme: themeReducer,
   drawer: drawerReducer,
+  [mbbrApi.reducerPath]: mbbrApi.reducer,
 });
+
+export const apis = [mbbrApi];
+
+export const apiReducerPaths = apis.map((api) => api.reducerPath);
 
 export default rootReducer;
