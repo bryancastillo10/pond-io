34b14aeb42b8a6af4998a08b6cd877d8c19034fa
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-10-17 13:00:37 +0800

    📝 form: second stage form

--- client/src/features/mbbr/components/MBBRSecondStageForm.tsx
+++ client/src/features/mbbr/components/MBBRSecondStageForm.tsx
@@ -1,10 +1,64 @@
+import { useMBBRFormContext } from "@/features/mbbr/context/MBBRFormContext";
+import useDrawer from "@/lib/drawer-ui/useDrawer";
+
+import NumberInput from "@/components/ui/NumberInput";
+import Button from "@/components/ui/Button";
+
 const MBBRSecondStageForm = () => {
+  const { handleCloseDrawer } = useDrawer();
+
+  const { secondStageData, handleSave, handleChange } = useMBBRFormContext();
+
   return (
-    <div className="">
-      <h1>MBBR Second Stage Unit</h1>
-      <p>Nitrification Tank</p>
-      <form></form>
-    </div>
+    <form className="grid grid-cols-1">
+      <NumberInput
+        label="Surface Area Loading Rate"
+        value={secondStageData.salr}
+        onChange={handleChange("secondStageData", "salr")}
+        decimals={0}
+        disabled={false}
+        unit="g/m²-d"
+      />
+
+      <NumberInput
+        label="Carrier Fill"
+        value={secondStageData.carrierFill}
+        onChange={handleChange("secondStageData", "carrierFill")}
+        decimals={0}
+        disabled={false}
+        unit="%"
+      />
+
+      <NumberInput
+        label="Liquid Depth"
+        value={secondStageData.waterLevel}
+        onChange={handleChange("firstStageData", "waterLevel")}
+        decimals={2}
+        disabled={false}
+        unit="m"
+      />
+
+      <NumberInput
+        label="Carrier Surface Area"
+        value={secondStageData.carrierSurfaceArea}
+        onChange={handleChange("firstStageData", "carrierSurfaceArea")}
+        decimals={2}
+        disabled={false}
+        unit="m²/m³"
+      />
+
+      <div className="flex justify-between gap-4 w-fit">
+        <Button action={handleCloseDrawer} variant="outline">
+          Cancel
+        </Button>
+        <Button
+          action={() => handleSave("secondStageData", handleCloseDrawer)}
+          variant="primary"
+        >
+          Save
+        </Button>
+      </div>
+    </form>
   );
 };
 
