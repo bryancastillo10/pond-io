317fa879f975b73d32d315558deae877498396f4
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-10-27 09:54:23 +0800

    🧩 component: Septic Tank diagram input buttons

--- client/src/features/septic_tank/components/Diagram.tsx
+++ client/src/features/septic_tank/components/Diagram.tsx
@@ -1,13 +1,32 @@
 import useSepticTankDiagram from "@/features/septic_tank/hooks/useSepticTankDiagram";
 
+import EditFormButton from "@/components/ui/EditFormButton";
 import StartSimulationButton from "@/components/ui/StartSimulationButton";
 
 const Diagram = () => {
   const { septicTankRef } = useSepticTankDiagram();
 
   return (
-    <div className="relative rounded-lg border shadow-md my-2 w-full h-[75vh] mx-auto flex justify-center items-center">
+    <div className="relative rounded-lg oveflow-x-scroll border shadow-md my-2 w-full h-[75vh] mx-auto flex justify-center items-center">
+      <EditFormButton
+        openDrawer={() => {}}
+        position="absolute left-[10%] md:left-[28%] top-[20%]"
+        isFormCompleted={false}
+      />
+
+      <EditFormButton
+        openDrawer={() => {}}
+        position="absolute left-[30%] md:left-[38%] top-[20%]"
+        isFormCompleted={false}
+      />
+
       <canvas ref={septicTankRef} width={1000} height={400} />
+
+      <EditFormButton
+        openDrawer={() => {}}
+        position="absolute right-[28%] top-[20%]"
+        isFormCompleted={false}
+      />
 
       <StartSimulationButton handleStart={() => {}} />
     </div>
