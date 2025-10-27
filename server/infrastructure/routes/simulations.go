53aac3c91cf6e893220c00e5721f8764ab39d70a
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-10-27 15:25:41 +0800

    🌐api: connect simulate uasb to routes

--- server/infrastructure/routes/simulations.go
+++ server/infrastructure/routes/simulations.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"pond-io-server/internal/mbbr"
 	septictank "pond-io-server/internal/septic_tank"
+	"pond-io-server/internal/uasb"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,12 +11,13 @@ import (
 func RegisterSimulationRoutes (r *gin.Engine) {
 	mbbrHandler := mbbr.NewHandler()
 	septicTankHandler := septictank.NewHandler()
+	uasbHandler := uasb.NewHandler()
 
 	simulationGroup := r.Group("/simulate") 
 	{
 		simulationGroup.POST("/mbbr",mbbrHandler.SimulateMBBR)
 		simulationGroup.POST("/ad")
 		simulationGroup.POST("/septic-tank", septicTankHandler.SimulateSepticTank)
-		simulationGroup.POST("/uasb")
+		simulationGroup.POST("/uasb", uasbHandler.SimulateUASB)
 	}
 }
