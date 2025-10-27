5109fc8948e3ef2e6120777b4c2d98cc7dfe07ee
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-10-27 09:06:23 +0800

    🧹 chore: absolute positions

--- client/src/features/mbbr/components/Diagram.tsx
+++ client/src/features/mbbr/components/Diagram.tsx
@@ -18,29 +18,29 @@ const Diagram = () => {
   const { formCompletion, handleSimulate } = useMBBRFormContext();
 
   return (
-    <div className="relative rounded-lg border shadow-md my-2 w-full h-[75vh] mx-auto flex justify-center items-center">
+    <div className="relative rounded-lg border overflow-x-scroll shadow-md my-2 w-full h-[75vh] mx-auto flex justify-center items-center">
       <EditFormButton
         openDrawer={openInlfuentForm}
-        position="top-25 md:top-40 left-30"
+        position="absolute left-[5%] top-[10%] md:top-[25%] md:left-[10%] lg:top-[15%]"
         isFormCompleted={formCompletion.infData}
       />
 
       <EditFormButton
         openDrawer={openFirstStageForm}
-        position="top-20 left-100"
+        position="absolute left-[30%] top-[10%] md:top-[20%] md:left-[45%] lg:top-[15%]"
         isFormCompleted={formCompletion.firstStageData}
       />
 
       <EditFormButton
         openDrawer={openSecondStageForm}
-        position="top-20 right-120"
+        position="absolute top-[10%] right-[10%] md:top-[20%] md:right-[20%] lg:top-[15%]"
         isFormCompleted={formCompletion.secondStageData}
       />
       <canvas ref={mbbrRef} width={1000} height={450} />
 
       <EditFormButton
         openDrawer={openEffluentForm}
-        position="bottom-60 right-40"
+        position="absolute bottom-[15%] right-[15%]"
         isFormCompleted={formCompletion.effData}
       />
       <StartSimulationButton handleStart={handleSimulate} />
