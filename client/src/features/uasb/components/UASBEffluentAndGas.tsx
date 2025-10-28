218ed03ea88496210b0e498c020bc327d194937d
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-10-28 13:32:51 +0800

    📝 form: UASB parameter inputs

--- client/src/features/uasb/components/UASBEffluentAndGas.tsx
+++ client/src/features/uasb/components/UASBEffluentAndGas.tsx
@@ -1,8 +1,44 @@
+import NumberInput from "@/components/ui/NumberInput";
+import Button from "@/components/ui/Button";
+
 const UASBEffluentAndGas = () => {
   return (
-    <div>
-      <h1>UASB Effluent and Gas Properties Input</h1>
-    </div>
+    <form className="grid grid-cols-1">
+      <NumberInput
+        label="Percentage COD Removal"
+        value=""
+        onChange={() => {}}
+        decimals={0}
+        disabled={false}
+        unit="%"
+      />
+
+      <NumberInput
+        label="Methane Yield"
+        value=""
+        onChange={() => {}}
+        disabled={false}
+        unit="m³ CH₄/ kg COD"
+      />
+
+      <NumberInput
+        label="Methane Fraction in Biogas"
+        value=""
+        onChange={() => {}}
+        decimals={1}
+        disabled={false}
+        unit=""
+      />
+
+      <div className="flex justify-between gap-4 w-fit">
+        <Button action={() => {}} variant="danger">
+          Cancel
+        </Button>
+        <Button action={() => {}} variant="primary">
+          Save
+        </Button>
+      </div>
+    </form>
   );
 };
 
--- client/src/features/uasb/components/UASBOperationParameters.tsx
+++ client/src/features/uasb/components/UASBOperationParameters.tsx
@@ -1,8 +1,52 @@
+import NumberInput from "@/components/ui/NumberInput";
+import Button from "@/components/ui/Button";
+
 const UASBOperationParameters = () => {
   return (
-    <div>
-      <h1>Water Quality and Operational Parameters Input of UASB</h1>
-    </div>
+    <form className="grid grid-cols-1">
+      <NumberInput
+        label="Flow Rate"
+        value=""
+        onChange={() => {}}
+        disabled={false}
+        unit="m³/d"
+      />
+
+      <NumberInput
+        label="Influent COD"
+        value=""
+        onChange={() => {}}
+        disabled={false}
+        unit="mg/L"
+      />
+
+      <NumberInput
+        label="Organic Loading Rate"
+        value=""
+        onChange={() => {}}
+        decimals={2}
+        disabled={false}
+        unit="kg COD/m³ d"
+      />
+
+      <NumberInput
+        label="Upflow Velocity"
+        value=""
+        onChange={() => {}}
+        decimals={2}
+        disabled={false}
+        unit="m/h"
+      />
+
+      <div className="flex justify-between gap-4 w-fit">
+        <Button action={() => {}} variant="danger">
+          Cancel
+        </Button>
+        <Button action={() => {}} variant="primary">
+          Save
+        </Button>
+      </div>
+    </form>
   );
 };
 
