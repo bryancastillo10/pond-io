7d08d5b93285a454a471b105359870c27189f9ba
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-10-29 15:29:43 +0800

    🧹 chore: register uasb api reducer at redux

--- client/src/lib/redux/reducer.ts
+++ client/src/lib/redux/reducer.ts
@@ -6,14 +6,16 @@ import drawerReducer from "@/lib/redux/slice/drawerSlice";
 
 // API from RTK-Query
 import { mbbrApi } from "@/features/mbbr/api/simulate";
+import { uasbApi } from "@/features/uasb/api/simulate";
 
 const rootReducer = combineReducers({
   theme: themeReducer,
   drawer: drawerReducer,
   [mbbrApi.reducerPath]: mbbrApi.reducer,
+  [uasbApi.reducerPath]: uasbApi.reducer,
 });
 
-export const apis = [mbbrApi];
+export const apis = [mbbrApi, uasbApi];
 
 export const apiReducerPaths = apis.map((api) => api.reducerPath);
 
