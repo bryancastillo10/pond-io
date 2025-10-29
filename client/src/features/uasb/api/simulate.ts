81057ed7d4f384b42e6fa6739facfaca7f361029
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-10-29 13:47:02 +0800

    🌐 api: fetch simulateUASB

---
+++ client/src/features/uasb/api/simulate.ts
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+import type {
+  SimulateUASBRequest,
+  SimulateUASBResponse,
+} from "@/features/uasb/api/interface";
+
+export const uasbApi = createApi({
+  reducerPath: "uasbApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_API_BASE_URL + "simulate",
+  }),
+  endpoints: (build) => ({
+    simulateUasb: build.mutation<SimulateUASBResponse, SimulateUASBRequest>({
+      query: (data) => ({
+        url: "/mbbr",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useSimulateUasbMutation } = uasbApi;
