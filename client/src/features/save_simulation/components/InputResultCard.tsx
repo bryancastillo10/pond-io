70ea1f9f565f13049e02b725e39af84cd95a5f29
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-10-31 13:31:55 +0800

    🧩component: InputResultCard

---
+++ client/src/features/save_simulation/components/InputResultCard.tsx
@@ -0,0 +1,22 @@
+import { useAppSelector } from "@/lib/redux/hooks";
+
+import { renderObject } from "@/utils/renderObject";
+
+const InputResultCard = (input: { input: Record<string, any> }) => {
+  const isDarkMode = useAppSelector((state) => state.theme.isDarkMode);
+
+  return (
+    <div>
+      <h2 className="font-semibold mb-2">Input Parameters</h2>
+      <div
+        className={`p-4 rounded-lg border border-text ${
+          isDarkMode ? "bg-accent" : "bg-gray-50"
+        }`}
+      >
+        {renderObject(input)}
+      </div>
+    </div>
+  );
+};
+
+export default InputResultCard;
