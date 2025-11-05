a9cd59c2d386a3ead50ab20a63125f232ab3414b
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-11-05 14:57:13 +0800

    ✨ feature: simulation record query

--- client/src/components/pages/SimulationRecords.tsx
+++ client/src/components/pages/SimulationRecords.tsx
@@ -1,7 +1,37 @@
+import { useGetSimulationQuery } from "@/features/records/api/recordsApi";
+
 const SimulationRecords = () => {
+  const {
+    data: simulationRecords,
+    isLoading,
+    isError,
+  } = useGetSimulationQuery();
+
+  if (isLoading) {
+    return (
+      <div className="">
+        <p>Loading Simulation Records</p>
+      </div>
+    );
+  }
+
+  if (isError || !simulationRecords) {
+    return (
+      <div className="">
+        <p>Error: No Simulation Records Recovered</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>Simulation Records Here</h1>
+
+      <div>
+        {simulationRecords?.records.map((record) => (
+          <p key={record.id}>{record.title}</p>
+        ))}
+      </div>
     </div>
   );
 };
