6e8b6aa0058fa8474e9db4982d94c7fffcab4e2d
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-11-10 14:48:23 +0800

    🧹 chore: add icon and adjust text theme on cards ui

--- client/src/features/records/components/InputResultCard.tsx
+++ client/src/features/records/components/InputResultCard.tsx
@@ -1,13 +1,17 @@
 import { useAppSelector } from "@/lib/redux/hooks";
 
+import { TextCursorInput } from "lucide-react";
 import { renderObject } from "@/utils/renderObject";
 
 const InputResultCard = ({ input }: { input: Record<string, any> }) => {
   const isDarkMode = useAppSelector((state) => state.theme.isDarkMode);
 
   return (
-    <div>
-      <h2 className="font-semibold mb-2">Input Parameters</h2>
+    <div className={`${isDarkMode ? "text-background" : "text-text"}`}>
+      <div className="flex items-center gap-2 my-2 text-background">
+        <TextCursorInput />
+        <h2 className="font-semibold">Input Parameters</h2>
+      </div>
       <div
         className={`p-4 rounded-lg border border-text ${
           isDarkMode ? "bg-accent" : "bg-gray-50"
