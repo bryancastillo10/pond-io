7955a3560c9ff2abd375f6f805698613a37d394f
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-11-17 09:45:59 +0800

    ✨feature: form drawer usage of septic tank

--- client/src/features/septic_tank/components/Diagram.tsx
+++ client/src/features/septic_tank/components/Diagram.tsx
@@ -1,3 +1,4 @@
+import useSepticTankFormDrawers from "@/features/septic_tank/hooks/useSepticTankFormDrawers";
 import useSepticTankDiagram from "@/features/septic_tank/hooks/useSepticTankDiagram";
 
 import EditFormButton from "@/components/ui/EditFormButton";
@@ -6,24 +7,21 @@ import StartSimulationButton from "@/components/ui/StartSimulationButton";
 const Diagram = () => {
   const { septicTankRef } = useSepticTankDiagram();
 
+  const { openWaterQualityForm, openSludgeCharacteristicsForm } =
+    useSepticTankFormDrawers();
+
   return (
     <div className="relative rounded-lg oveflow-x-scroll border shadow-md my-2 w-full h-[75vh] mx-auto flex justify-center items-center">
       <EditFormButton
-        openDrawer={() => {}}
+        openDrawer={openWaterQualityForm}
         position="absolute left-[10%] md:left-[28%] top-[20%]"
         isFormCompleted={false}
       />
 
-      {/* <EditFormButton
-        openDrawer={() => {}}
-        position="absolute left-[30%] md:left-[38%] top-[20%]"
-        isFormCompleted={false}
-      /> */}
-
       <canvas ref={septicTankRef} width={1000} height={400} />
 
       <EditFormButton
-        openDrawer={() => {}}
+        openDrawer={openSludgeCharacteristicsForm}
         position="absolute right-[28%] top-[20%]"
         isFormCompleted={false}
       />
