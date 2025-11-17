4a0e7d3e3e023875613aa2d0c05826949969c2a7
Bryan Castillo <64648492+bryancastillo10@users.noreply.github.com>
2025-11-17 09:10:17 +0800

    ✨ feature: register septic tank api to redux reducer

--- client/src/lib/redux/reducer.ts
+++ client/src/lib/redux/reducer.ts
@@ -7,6 +7,7 @@ import drawerReducer from "@/lib/redux/slice/drawerSlice";
 // API from RTK-Query
 import { mbbrApi } from "@/features/mbbr/api/simulate";
 import { uasbApi } from "@/features/uasb/api/simulate";
+import { septicTankApi } from "@/features/septic_tank/api/simulate";
 
 import { recordsApi } from "@/features/records/api/recordsApi";
 
@@ -15,10 +16,11 @@ const rootReducer = combineReducers({
   drawer: drawerReducer,
   [mbbrApi.reducerPath]: mbbrApi.reducer,
   [uasbApi.reducerPath]: uasbApi.reducer,
+  [septicTankApi.reducerPath]: septicTankApi.reducer,
   [recordsApi.reducerPath]: recordsApi.reducer,
 });
 
-export const apis = [mbbrApi, uasbApi, recordsApi];
+export const apis = [mbbrApi, uasbApi, septicTankApi, recordsApi];
 
 export const apiReducerPaths = apis.map((api) => api.reducerPath);
 
